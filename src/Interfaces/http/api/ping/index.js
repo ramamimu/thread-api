c50e5fcd98e8a25a97ffff259d64c916ecc3fff1
ramamimu <ahm.f.ramadhani@gmail.com>
2024-02-16 21:20:06 +0700

    fix: script

--- src/Interfaces/http/api/ping/index.js
+++ src/Interfaces/http/api/ping/index.js
@@ -11,7 +11,7 @@ module.exports = {
           return h
             .response({
               status: "success",
-              message: "pong",
+              message: "ping and pong",
             })
             .code(200);
         },
